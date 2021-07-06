7f963cf5014a66084031c63ffa5d28bbe9e948dd
Mayurdhvaj15081996 <rajmayurdhvaj9@gmail.com>
2021-07-06 21:36:04 +0530

    Edit In SignUp.js
    as well as in index.html

---
+++ src/RightPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+export default function SideMenu() {
+    return (
+        <div className = "Header">
+            <Header/>
+            <div className="Container">
+                <div className="SideMenu">
+
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import forMainPageImage from './forMainPage.png';
 
@@ -10,15 +11,20 @@ export default function Login() {
             <img src={forMainPageImage} width="600" height="500" className="Logo"></img> <br></br>
             </div> 
             <div className = "Right">
-                <div className="g-signin2" data-onsuccess={onSuccess}></div>
+                <div className="Notice_Login">
+                    Login with your Parul University Google Email ONLY. <br/>Sign out of any other google account on your Chrome browser <br/>before clicking on the Sign in with Google button.<br/>
+                </div>
+                <div className="g-signin2" data-onsuccess={onSuccess} data-width="250" data-height="50" data-longtitle="true"></div>
             </div>
             <div className="ForBlank">
-
+                <div className ="Button">
+                </div>
             </div>
         </div>
     );
 }
-
+  
 function onSuccess(googleUser) {
-    console.log('Logged in as: ' + JSON.stringify(googleUser.getBasicProfile().getName()));
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
   }
+
